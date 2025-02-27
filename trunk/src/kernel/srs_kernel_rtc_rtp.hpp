418d9aa101f56bb309823f6f6144d90666806c49
Haibo Chen <495810242@qq.com>
2025-02-27 16:07:36 +0800

    rtc2rtmp: support hevc

--- trunk/src/kernel/srs_kernel_rtc_rtp.hpp
+++ trunk/src/kernel/srs_kernel_rtc_rtp.hpp
@@ -259,6 +259,7 @@ enum SrsRtspPacketPayloadType
     SrsRtspPacketPayloadTypeFUA2,
     SrsRtspPacketPayloadTypeFUAHevc2,
     SrsRtspPacketPayloadTypeFUA,
+    SrsRtspPacketPayloadTypeFUAHevc2,
     SrsRtspPacketPayloadTypeFUAHevc,
     SrsRtspPacketPayloadTypeNALU,
     SrsRtspPacketPayloadTypeSTAP,
@@ -342,6 +343,7 @@ public:
     virtual srs_error_t decode(SrsBuffer* buf);
 public:
     bool is_keyframe();
+    bool is_h265();
     // Get and set the packet sync time in milliseconds.
     void set_avsync_time(int64_t avsync_time) { avsync_time_ = avsync_time; }
     int64_t get_avsync_time() const { return avsync_time_; }
